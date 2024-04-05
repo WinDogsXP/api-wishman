2aa01a3d49fdd5a6e37efd6f0492a8292efdabfe
Dragan Iulian <iulianalexandrudragan@gmail.com>
2024-04-05 19:07:38 +0300

    refactor a bit

--- server/attach.js
+++ server/attach.js
@@ -1,9 +1,13 @@
+function report_status(status, endpoint) {
+  console.log(status);
+}
+
 function watchdog(endpoint) {
   setInterval(() => {
     fetch(endpoint.url)
-      .then((res) => console.log(`${endpoint.url}: ${res.ok}`))
+      .then((res) => report_status(res.status, endpoint))
       .catch(() => {
-        console.log(`${endpoint.url}: false`);
+        report_status(false);
       });
   }, endpoint.timeout_duration);
 }
@@ -11,3 +15,4 @@ function watchdog(endpoint) {
 module.exports = {
   watchdog: watchdog,
 };
+
